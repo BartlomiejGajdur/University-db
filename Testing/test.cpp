25db7f28241510aeaa27d4a9fbfd9387dd2c2a05
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-12-08 21:25:29 +0100

    Test for functionality 'add'

--- Testing/test.cpp
+++ Testing/test.cpp
@@ -1,1 +1,26 @@
+
+#include <memory>
+
+#include "../Include/Student.hpp"
+#include "../Include/Database.hpp"
+
 #include "gtest/gtest.h"
+
+TEST(CheckStructure, CanAddStudentToDb){
+    Student adam{
+        "Adam",
+        "Kowalski",
+        "Krakow 21",
+        0,
+        "000000000",
+        Gender::Male
+    };
+
+    Database db;
+
+    size_t sizeBeforeAdd = db.getVectorOfStudents().size();
+    db.add(std::make_shared<Student>(adam));
+    size_t sizeAfterAdd = db.getVectorOfStudents().size();
+
+    EXPECT_EQ(sizeBeforeAdd,sizeAfterAdd-1);
+}
