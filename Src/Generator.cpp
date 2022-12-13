60de89f6fdf8e666742d55234e77c40435e4a4f3
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-12-13 16:09:15 +0100

    Generate Pesel added

--- Src/Generator.cpp
+++ Src/Generator.cpp
@@ -1,6 +1,115 @@
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <vector>
 #include "../Include/Generator.hpp"
+#include "../Include/validatePesel.hpp"
+
+int generate::generateRandomNumber(const int& first, const int& second){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> random(first,second);
+
+    return random(gen);
+}
+
+std::string generate::generateDay(const std::string& yearAndMonth){
+
+    std::string day;
+
+    Month ENUMmonth = static_cast<Month>(std::stoi(yearAndMonth) % 20);
+    
+    if((ENUMmonth == Month::January      ||
+            ENUMmonth == Month::March    ||
+            ENUMmonth == Month::May      ||
+            ENUMmonth == Month::July     ||
+            ENUMmonth == Month::August   ||
+            ENUMmonth == Month::October  ||
+            ENUMmonth == Month::December))
+            {
+                day = std::to_string(generate::generateRandomNumber(0,31));
+                    if(day.size() == 1)
+                    {
+                        day+="0";
+                        reverse(day.begin(),day.end());
+                    }
+            }
+
+    
+    if((ENUMmonth == Month::April         ||
+            ENUMmonth == Month::June      ||
+            ENUMmonth == Month::September ||
+            ENUMmonth == Month::November))
+            {
+                day = std::to_string(generate::generateRandomNumber(0,30));
+                    if(day.size() == 1)
+                    {
+                        day+="0";
+                        reverse(day.begin(),day.end());
+                    }
+            }
+
+    if(PeselValidator::checkLeapYear(yearAndMonth) && ENUMmonth == Month::February) {
+                day = std::to_string(generate::generateRandomNumber(0,29));
+                    if(day.size() == 1)
+                    {
+                        day+="0";
+                        reverse(day.begin(),day.end());
+                    }
+            }
+
+    if(!PeselValidator::checkLeapYear(yearAndMonth) && ENUMmonth == Month::February){
+                day = std::to_string(generate::generateRandomNumber(0,28));
+                    if(day.size() == 1)
+                    {
+                        day+="0";
+                        reverse(day.begin(),day.end());
+                    }
+                }
+
+    return day;
+}
+
 
 std::string generate::generatePesel(){
+    std::string year,month,day,digits,pesel;
+    int controlDigit;
 
+    //Generate year
+    year = std::to_string(generate::generateRandomNumber(0,99));
+    if(year.size() == 1)
+        year+=std::to_string(generate::generateRandomNumber(0,9));
 
+    //Generate Month
+    month = std::to_string(generate::generateRandomNumber(1,32));
+
+    while(month>="13" && month<="20"){
+        month = std::to_string(generate::generateRandomNumber(1,32));
+    }
+    if(month.size() == 1){
+        month+="0";
+        reverse(month.begin(),month.end());
+    }
+
+    //Generate day
+    day = generate::generateDay(year+month);
+        
+    //Generate 4 digits
+    for(int i =0 ; i<4 ;i++){
+        digits+=std::to_string(generate::generateRandomNumber(0,9));
+    }
+
+    //Generate controlDigit
+    std::vector<int> wages{1,3,7,9,1,3,7,9,1,3};
+    pesel = year + month + day + digits;
+    auto Operation = [](const int& wage, char zn){
+      return wage * (int(zn)-48);
+    };
+
+    controlDigit = std::transform_reduce(wages.begin(),wages.end(),pesel.begin(),0,std::plus<>(),Operation);
+    controlDigit = controlDigit % 10;
+    if(controlDigit != 0)
+        controlDigit=10-controlDigit;
+
+    return pesel + std::to_string(controlDigit);
 }
--- Src/main.cpp
+++ Src/main.cpp
@@ -5,135 +5,143 @@
 #include "../Include/validatePesel.hpp"
 #include "../Include/Person.hpp"
 #include "../Include/Employee.hpp"
+#include "../Include/Generator.hpp"
 
 int main(){ 
 
-     Student Adam{
-        "Adam",
-        "bowalski",
-        "Krakow 21",
-        "95081685721"   };
+    //  Student Adam{
+    //     "Adam",
+    //     "bowalski",
+    //     "Krakow 21",
+    //     "95081685721"   };
 
-    Student Kasia{
-        "Kasia",
-        "calkowski",
-        "Krakow 21dd",
-        "89032892537"
-    };
+    // Student Kasia{
+    //     "Kasia",
+    //     "calkowski",
+    //     "Krakow 21dd",
+    //     "89032892537"
+    // };
 
-    Student Bartek{
-        "Kasia",
-        "Malkowski",
-        "Krakow 21dd",
-        "05041681881"
-    };
-     Student Adam1{
-        "Adam",
-        "bo3232walski",
-        "Krakow 21",
-        "53050719156"   };
+    // Student Bartek{
+    //     "Kasia",
+    //     "Malkowski",
+    //     "Krakow 21dd",
+    //     "05041681881"
+    // };
+    //  Student Adam1{
+    //     "Adam",
+    //     "bo3232walski",
+    //     "Krakow 21",
+    //     "53050719156"   };
 
-    Student Kasia1{
-        "Kasia",
-        "calkowski",
-        "Krakow 21dd",
-        "00111555121"
-    };
+    // Student Kasia1{
+    //     "Kasia",
+    //     "calkowski",
+    //     "Krakow 21dd",
+    //     "00111555121"
+    // };
 
-    Employee Bartek1{
-        "Krzysiek",
-        "Osuch",
-        "Krakow 21dd",
-        "53081891465",
-        5000
-    };
+    // Employee Bartek1{
+    //     "Krzysiek",
+    //     "Osuch",
+    //     "Krakow 21dd",
+    //     "53081891465",
+    //     5000
+    // };
 
-    Employee Bartek2{
-        "Krzysztof",
-        "Gajdur",
-        "Warszawa 21dd",
-        "53081891465",
-        7000
-    };
+    // Employee Bartek2{
+    //     "Krzysztof",
+    //     "Gajdur",
+    //     "Warszawa 21dd",
+    //     "53081891465",
+    //     7000
+    // };
 
-    Employee Bartek3{
-        "Karmowski",
-        "Malkowski",
-        "Warszawa 21dd",
-        "53081891465",
-        2400
-    };
+    // Employee Bartek3{
+    //     "Karmowski",
+    //     "Malkowski",
+    //     "Warszawa 21dd",
+    //     "53081891465",
+    //     2400
+    // };
 
-    Student Kasia5{
-        "Dodane nowe",
-        "calkowski",
-        "Krakow 21dd",
-        "00111555121"
-    };
+    // Student Kasia5{
+    //     "Dodane nowe",
+    //     "calkowski",
+    //     "Krakow 21dd",
+    //     "00111555121"
+    // };
 
-    Employee Bartek5{
-        "Krzysiek",
-        "Os323232uch",
-        "Krakow 21dd",
-        "53081891465",
-        4444
-    };
+    // Employee Bartek5{
+    //     "Krzysiek",
+    //     "Os323232uch",
+    //     "Krakow 21dd",
+    //     "53081891465",
+    //     4444
+    // };
 
-    Database db;
+    // Database db;
 
-    db.add(std::make_shared<Student>(Adam));
-    db.add(std::make_shared<Student>(Kasia));
-    db.add(std::make_shared<Employee>(Bartek2)); 
-    db.add(std::make_shared<Student>(Bartek)); 
-    db.add(std::make_shared<Student>(Adam1));
-    db.add(std::make_shared<Employee>(Bartek3)); 
-    db.add(std::make_shared<Student>(Kasia1));
-    db.add(std::make_shared<Employee>(Bartek1)); 
-    db.add(Bartek5);
-    db.add(Kasia5);
+    // db.add(std::make_shared<Student>(Adam));
+    // db.add(std::make_shared<Student>(Kasia));
+    // db.add(std::make_shared<Employee>(Bartek2)); 
+    // db.add(std::make_shared<Student>(Bartek)); 
+    // db.add(std::make_shared<Student>(Adam1));
+    // db.add(std::make_shared<Employee>(Bartek3)); 
+    // db.add(std::make_shared<Student>(Kasia1));
+    // db.add(std::make_shared<Employee>(Bartek1)); 
+    // db.add(Bartek5);
+    // db.add(Kasia5);
 
-    db.printDatabase();
-
-    // db.sortBySurname(Order::Ascending);
     // db.printDatabase();
 
-    // db.sortBySurname(Order::Descending);
-    // db.printDatabase();
+    // // db.sortBySurname(Order::Ascending);
+    // // db.printDatabase();
 
-    // std::cout<<"\n\n@@@@@\n\n";
+    // // db.sortBySurname(Order::Descending);
+    // // db.printDatabase();
 
-    //  db.sortByPesel(Order::Ascending);
-    // db.printDatabase();
+    // // std::cout<<"\n\n@@@@@\n\n";
 
-    // db.sortByPesel(Order::Descending);
-    // db.printDatabase();
+    // //  db.sortByPesel(Order::Ascending);
+    // // db.printDatabase();
+
+    // // db.sortByPesel(Order::Descending);
+    // // db.printDatabase();
     
-    // std::string Pesel{"92022919156"};
-    // if(PeselValidator::checkLeapYear(Pesel))
-    //     std::cout<<"IsLeap\n";
+    // // std::string Pesel{"92022919156"};
+    // // if(PeselValidator::checkLeapYear(Pesel))
+    // //     std::cout<<"IsLeap\n";
 
-    // if(PeselValidator::checkDate(Pesel))
-    //     std::cout<<"Dobra data\n";
+    // // if(PeselValidator::checkDate(Pesel))
+    // //     std::cout<<"Dobra data\n";
 
-    // if(PeselValidator::checkDigit("55030101193"))
-    //     std::cout<<"ZGADZA SIE PESEL!\n";
+    // // if(PeselValidator::checkDigit("55030101193"))
+    // //     std::cout<<"ZGADZA SIE PESEL!\n";
     
-    // if(PeselValidator::checkDigit("55030101230"))
-    //     std::cout<<"Zgadza SIe pesel!\n";
+    // // if(PeselValidator::checkDigit("55030101230"))
+    // //     std::cout<<"Zgadza SIe pesel!\n";
 
-    // if(PeselValidator::validatePesel("62080697415"))
-    //     std::cout<<"ZGADZA SIE PESEL!\n";
+    // // if(PeselValidator::validatePesel("62080697415"))
+    // //     std::cout<<"ZGADZA SIE PESEL!\n";
 
-    // std::vector<std::shared_ptr<Person>> finnaly = db.findBySurname("Malkowski");
-    // //testy:
-    // for (auto vec : finnaly){
-    //     std::cout<<*vec<<"\n";
-    // }
-    // db.removeByIndex(1);
-    //  db.printDatabase();
+    // // std::vector<std::shared_ptr<Person>> finnaly = db.findBySurname("Malkowski");
+    // // //testy:
+    // // for (auto vec : finnaly){
+    // //     std::cout<<*vec<<"\n";
+    // // }
+    // // db.removeByIndex(1);
+    // //  db.printDatabase();
 
-    db.saveDatabaseToFile("DatabaseOfStudents");
+    // db.saveDatabaseToFile("DatabaseOfStudents");
     
-     std::cout<<*(db.findByPesel("0504123681881"));
+    //  std::cout<<*(db.findByPesel("0504123681881"));
+
+   if(PeselValidator::validatePesel(generate::generatePesel())){
+        std::cout<<"Jest super ten pesel!\n";
+   }else{
+        std::cout<<"ojjj\n";
+   }
+
     return 0;
 }
