86c14d775fc04383be9e66f43926299bbc8f24fb
Bartlomiej Gajdur <gajdurbartlomiej@gmail.com>
2022-12-17 14:24:25 +0100

    tests for leap year added

--- Testing/ValidatePeselTESTS.cpp
+++ Testing/ValidatePeselTESTS.cpp
@@ -39,3 +39,36 @@ INSTANTIATE_TEST_SUITE_P(Pesels,
                          CheckPesel,
                          testing::ValuesIn(PeselsWithThierResponse));
 
+
+std::vector<std::pair<std::string, bool>> LeapYearWithThierResponse = {
+    {"48040885491", true},  //1948
+    {"80120512467", true}, //1980
+    {"0021", true},         //2000
+    {"1621", true},         //2016
+    {"2821", true},         //2028
+    {"0412", true},         //1904
+    {"47051218130", false}, //1947
+    {"", false},
+    {"0001", false},        //1900
+    {"xxxx", false},
+    {"12x2", false},
+    {"xx02", false},
+    {"1903", false},        //1919
+    {"2201", false},        //1922
+    {"2921", false}         //2029
+   };
+
+
+class CheckLeapYear : public testing::TestWithParam<std::pair<std::string,bool>>{
+
+};
+
+TEST_P(CheckLeapYear, ValidateLeapYear){
+    auto [LeapYearToCheck, Response] = GetParam();
+
+    ASSERT_EQ(PeselValidator::checkLeapYear(LeapYearToCheck),Response);
+}
+
+INSTANTIATE_TEST_SUITE_P(Years,
+                         CheckLeapYear,
+                         testing::ValuesIn(LeapYearWithThierResponse));
